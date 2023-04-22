1455a155c7b49b6db5408fcd50c89b72a853dce8
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:14 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,64 @@
+<?php
+
+$this->add_right("smsreader", "account", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "account", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "account", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "account", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "account", "registered", view:true, add:true);
+$this->add_right("smsreader", "account", "guest", view:true, );
+
+$this->add_right("smsreader", "blacklist", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "blacklist", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "blacklist", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "blacklist", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "blacklist", "registered", view:true, add:true);
+$this->add_right("smsreader", "blacklist", "guest", view:true, );
+
+$this->add_right("smsreader", "confirming", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "confirming", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "confirming", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "confirming", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "confirming", "registered", view:true, add:true);
+$this->add_right("smsreader", "confirming", "guest", view:true, );
+
+$this->add_right("smsreader", "format", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "format", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "format", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "format", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "format", "registered", view:true, add:true);
+$this->add_right("smsreader", "format", "guest", view:true, );
+
+$this->add_right("smsreader", "incoming", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "incoming", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "incoming", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "incoming", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "incoming", "registered", view:true, add:true);
+$this->add_right("smsreader", "incoming", "guest", view:true, );
+
+$this->add_right("smsreader", "payment", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "payment", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "payment", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "payment", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "payment", "registered", view:true, add:true);
+$this->add_right("smsreader", "payment", "guest", view:true, );
+
+$this->add_right("smsreader", "requests", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "requests", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "requests", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "requests", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "requests", "registered", view:true, add:true);
+$this->add_right("smsreader", "requests", "guest", view:true, );
+
+$this->add_right("smsreader", "template", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "template", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "template", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "template", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "template", "registered", view:true, add:true);
+$this->add_right("smsreader", "template", "guest", view:true, );
+
+$this->add_right("smsreader", "whitelist", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "whitelist", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "whitelist", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("smsreader", "whitelist", "staff", view:true, add:true, edit:true);
+$this->add_right("smsreader", "whitelist", "registered", view:true, add:true);
+$this->add_right("smsreader", "whitelist", "guest", view:true, );
