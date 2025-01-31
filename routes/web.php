45042753c662d8234918c65f5c579f7e7e923f1f
Dedan <dedanirungu@gmail.com>
2025-01-31 20:41:37 +0300

    Remove uncessary routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Smsreader\Http\Controllers\IncomingController;
 
-Route::match (['get', 'post'], 'smsreader/incoming', 'IncomingController@incoming')->name('smsreader_incoming')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::get('smsreader/incoming/processor', 'IncomingController@incomingProcessor')->name('smsreader_incoming_processor');
-Route::post('smsreader/payment/verification', 'IncomingController@paymentVerification')->name('smsreader_payment_verification');
+Route::match(['get', 'post'], 'smsreader/incoming', [IncomingController::class, 'incoming'])->name('smsreader_incoming')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::get('smsreader/incoming/processor', [IncomingController::class, 'incomingProcessor'])->name('smsreader_incoming_processor');
+Route::post('smsreader/payment/verification', [IncomingController::class, 'paymentVerification'])->name('smsreader_payment_verification');
